9d9732c1ff90465d097a699fa3ccf119d17a6e4b
egreenri <epgreen@email.neit.edu>
2023-09-04 19:37:11 -0400

    axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
 import logo from "./logo.svg";
+import axios from "axios";
 import "./App.css";
 
 function App() {
   const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
+    // fetch("/api")
+    //   .then((res) => res.json())
+    //   .then((data) => setData(data.message));
+    test();
   }, []);
+
+  const test = async()=>{
+    const response = await axios.get(`/api`);
+
+    setData(response.data.message);
+  }
 
   return (
     <div className="App">
